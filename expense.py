3e05c0a7004a34f9c4409aa805c03484e403fd0a
mutxsim <almuatasamasseadi@gmail.com>
2024-02-01 11:09:01 -0500

    Deleted expense.py

--- expense.py
+++
@@ -1,8 +0,0 @@
-class Expense:
-    def __init__(self, name, amount, category):
-        self.name = name
-        self.amount = amount
-        self.category = category
-
-    def __str__(self):
-        return f"{self.name}, {self.amount}, {self.category}"
